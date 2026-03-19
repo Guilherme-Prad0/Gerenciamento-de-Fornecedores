e61c659f5bf79cb0ff6ad1fbabed1964073e9dcc
Guilherme <guilhermetprado@gmail.com>
2026-03-19 20:09:45 -0300

    Models criado

---
+++ fornecedores/src/main/java/com/zera/fornecedores/models/FornecedorModel.java
@@ -0,0 +1,52 @@
+package com.zera.fornecedores.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class FornecedorModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nomeFantasia;
+    private String cnpj;
+    private String contato;
+
+    public FornecedorModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+}
