7f70f47e8aa000de9f3910c1ca85c36a5a58e9da
Guilherme <guilhermetprado@gmail.com>
2026-03-19 20:12:28 -0300

    Repository criado

---
+++ fornecedores/src/main/java/com/zera/fornecedores/repositories/FornecedorRepository.java
@@ -0,0 +1,8 @@
+package com.zera.fornecedores.repositories;
+
+import com.zera.fornecedores.models.FornecedorModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepository extends JpaRepository<FornecedorModel, Long> {
+
+}
